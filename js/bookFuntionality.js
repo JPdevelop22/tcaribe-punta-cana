f33437b8ddecf33958167fd7424b6740b1abd0e5
Jose Otniel <joseotniel2010@gmail.com>
2021-08-03 12:54:12 -0400

    Initial commit: setted dependencies and try with some test

---
+++ js/bookFuntionality.js
@@ -0,0 +1,177 @@
+const travels = 
+{
+    "pickups": 
+    {
+        "Hoteles Bavaro Punta Cana": 
+        {
+            "name-ES": "Hoteles de la zona Bavaro, Punta Cana",
+            "name-EN": "Hotels in Bavaro, Punta Cana area",
+            "stops": [],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 2,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"] 			
+
+        },
+
+        "Hoteles Uvero Alto": 
+        {
+            "name-ES": "Hoteles en Uvero Alto",
+            "name-EN": "Hotels in Uvero Alto",
+            "stops": [],
+            "destiny-ES": ["PUJ", "La Romana", "Boca Chica-Aila"],
+            "destiny-EN": ["PUJ", "La Romana", "Boca Chica-Aila"],
+            "tracks": 2,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"]			
+
+        },
+
+        "Hoteles Higuey": 
+        { 
+            "name-ES": "Hoteles en Higüey",
+            "name-EN": "Hotels in Higüey",
+            "stops": [],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 2,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"]			
+
+        },
+
+        "Hoteles Bayahibe": 
+        {
+            "name-ES": "Hoteles en Bayahíbe",
+            "name-EN": "Hotels in Bayahíbe",
+            "stops": [],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 2,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"]			
+
+        },	
+
+        "Hoteles La Romana": 
+        { 
+            "name-ES": "Hoteles en La Romana",
+            "name-EN": "Hotels in La Romana",
+            "stops": [],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 2,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"]			
+
+        },	
+
+        "Hoteles Santana": 
+        {
+            "name-ES": "Hoteles en Santana Play.n",
+            "name-EN": "Hotels in Santana Play.n",
+            "stops": ["La Romana"],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 2,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"]			
+
+        },
+
+        "Hoteles Juan Dolio": 
+        {
+            "name-ES": "Hoteles en Juan Dolio",
+            "name-EN": "Hotels in Juan Dolio",
+            "stops": [],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 2,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"]			
+
+        },	
+    
+        "Hoteles Boca Chica": {
+            "name-ES": "Hoteles en Boca Chica",
+            "name-EN": "Hotels in Boca Chica",
+            "stops": [],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 2,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"]			
+
+        },
+
+        "Hoteles Santo Domingo": 
+        {
+            "name-ES": "Hoteles en Santo Domingo",
+            "name-EN": "Hotels in Santo Domingo",
+            "stops": [],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 2,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"]			
+
+        },
+
+        "Hoteles Santiago": 
+        {
+            "name-ES": "Hoteles en Santiago",
+            "name-EN": "Hotels in Santiago",
+            "stops": [],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 1,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33"] 			
+
+        },
+
+        "Hoteles Puerto Plata": 
+        {
+            "name-ES": "Hoteles en Puerto Plata",
+            "name-EN": "Hotels in Puerto Plata",
+            "stops": [],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 1,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"]			
+
+        },
+
+        "Hoteles Samana": 
+        {
+            "name-ES": "Hoteles en Samaná",
+            "name-EN": "Hotels in Samaná",
+            "stops": [],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 1,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33"] 			
+
+        },
+
+        "Hoteles Sabana de la Mar": 
+        {
+            "name-ES": "Hoteles en Sabana de la Mar",
+            "name-EN": "Hotels in Sabana de la Mar",
+            "stops": [],
+            "destiny-ES": ["PUJ"],
+            "destiny-EN": ["PUJ"],
+            "tracks": 2,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"]			
+
+        },
+
+        "PUJ": 
+        {
+            "name-ES": "Aeropuerto Internacional de Punta Cana",
+            "name-EN": "Punta Cana Internacional airport",
+            "stops": [],
+            "destiny-ES": ["Hoteles de la zona Bavaro, Punta Cana", "Hoteles en Uvero Alto", "Hoteles in La Romana", "San Pedro de Macorís", "El Seibo", "Boca Chica", "Santo Domingo", "Santiago", "Samaná", "Puerto Plata"],
+            "destiny-EN": ["Hotels in Bavaro, Punta Cana area", "Hotels in Uvero Alto", "Hotels in La Romana", "San Pedro de Macorís", "El Seibo", "Boca Chica", "Santo Domingo", "Santiago", "Samaná", "Puerto Plata"],
+            "tracks": 2,
+            "passengers": ["1 - 6", "7 - 21", "22 - 33", "34 - 45", "46 - 54", "55 - 59"]			
+        } 
+    }
+}
+
+let pickupsNames = Object.values(travels.pickups).map((pickup) => pickup["name-ES"])
+
+pickupsNames.forEach(element => {
+    console.log(element)
+});
