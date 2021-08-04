5653a6adbd46dad7a708c46e459f29bcf0fcdeea
Jose Otniel <joseotniel2010@gmail.com>
2021-08-03 22:17:14 -0400

    Book functionality implemented, now working on show recipt functionality

--- js/bookFuntionality.js
+++ js/bookFuntionality.js
@@ -170,8 +170,139 @@ const travels =
     }
 }
 
-let pickupsNames = Object.values(travels.pickups).map((pickup) => pickup["name-ES"])
+const pickups = Object.values(travels.pickups)
 
-pickupsNames.forEach(element => {
-    console.log(element)
-});
+const drawPickupsSelect = (pickups) => {
+    $("#startContainer").html(
+        '<label for="pickUpPoints">Punto de recogida</label>' +
+        '<select name="pickUpPoints" id="pickUpPoints" required>' +
+            '<option value=""></option>' +
+        '</select>'        
+    )
+
+    pickups.forEach((pickup) => {
+        let optionValue = `${pickup["name-ES"]} . ${pickup["stops"]} . ${pickup["destiny-ES"]} . ${pickup["tracks"]} . ${pickup["passengers"]}`
+        $('#pickUpPoints').append(
+            `<option value="${optionValue}">${pickup["name-ES"]}</option>`
+        )
+    })
+}
+
+const drawTracksSelect = (tracks) => {
+    $("#tracksContainer").html(
+        '<label for="tracks">Punto de recogida</label>' +
+        '<select name="tracks" id="tracks" required>' +
+            '<option value="1">1 via</option>' +
+        '</select>'        
+    )
+
+    if (tracks > 1) {
+        for (let i = 2; i <= tracks; i++) {
+            $('#tracks').append(
+                `<option value="${i}">${i} vias</option>`
+            )
+        }
+    }
+}
+
+const drawStopsSelect = (stops) => {
+    $("#stopContainer").html(
+        '<label for="stops">Punto de parada</label>' +
+        '<select name="stops" id="stops" required>' +
+            '<option value=""></option>' +
+        '</select>'        
+    )
+
+    stops.forEach(stop => {
+        $('#stops').append(
+            `<option value="${stop}">${stop}</option>`
+        )
+    })
+}
+
+const removeStopSelect = () => $('#stopContainer').empty()
+
+const drawDestinySelect = (destinies) => {
+    $("#destinyContainer").html(
+        '<label for="destinies">Destino</label>' +
+        '<select name="destinies" id="destinies" required>' +
+            '<option value=""></option>' +
+        '</select>'        
+    )
+
+    destinies.forEach(destiny => {
+        $('#destinies').append(
+            `<option value="${destiny}">${destiny}</option>`
+        )
+    })
+}
+
+const drawPassengersSelect = (passgersCount) => {
+    $("#passengersContainer").html(
+        '<label for="passengers">Pasajeros</label>' +
+        '<select name="passengers" id="passengers" required>' +
+            '<option value=""></option>' +
+        '</select>'        
+    )
+
+    passgersCount.forEach(passengers => {
+        $('#passengers').append(
+            `<option value="${passengers}">De ${passengers} pasajeros</option>`
+        )
+    })
+}
+
+$(document).ready(() => {
+    $('#bookType').on('change', (e) => {
+        if (e.target.value === 'viaje turistico') {
+            drawPickupsSelect(pickups)
+        }
+        console.log("changing...")
+    })
+    
+    $('#startContainer').on('change', '#pickUpPoints', (e) => {
+        const [name, stops, destiny, tracks, passengers] = e.target.value.split(' . ')
+
+        drawTracksSelect(tracks)
+
+        if (stops) {
+            drawStopsSelect(stops.split(","))
+        } else {
+            removeStopSelect()
+        }
+
+        console.log(destiny)
+        drawDestinySelect(destiny.split(","))
+
+        console.log(passengers)
+        drawPassengersSelect(passengers.split(","))
+    })
+
+    $('#bookForm').on('submit', (e) => {
+        e.preventDefault()
+
+        let message
+        if (e.target.stops) {
+            message = 
+                e.target.bookType.value + ' | ' +
+                e.target.tracks.value + ' | ' +
+                e.target.pickUpPoints.value + ' | ' +
+                e.target.stops.value + ' | ' +
+                e.target.destinies.value + ' | ' +
+                e.target.bookDate.value + ' | ' +
+                e.target.passengers.value
+        } else {
+            message = 
+                e.target.bookType.value + ' | ' +
+                e.target.tracks.value + ' | ' +
+                e.target.pickUpPoints.value + ' | ' +
+                e.target.destinies.value + ' | ' +
+                e.target.bookDate.value + ' | ' +
+                e.target.passengers.value
+        }
+
+        console.log(message)
+    })
+
+})
+
