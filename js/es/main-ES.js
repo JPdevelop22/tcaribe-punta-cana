844c7a463882d103fef8fe7b7cc0412b57c70d30
Jose Otniel <joseotniel2010@gmail.com>
2021-08-07 13:20:08 -0400

    Modified main again

--- js/es/main-ES.js
+++ js/es/main-ES.js
@@ -30,26 +30,28 @@ $(document).ready(() => {
 
     // On select excursion changing event handler 
     $("#excursionContainer").on('change', "#selectExcursions", evt => {
-        const [ _, passegers ] = evt.target.value.split(' . ') 
-        const passegersArr = passegers.split(',')
+        const [ _, passegers ] = evt.target.value.split(optionValueSeparator) 
+        const passegersArr = passegers.split(arraySeparator)
+        
         drawPassengersSelect(passegersArr)
+
+        emptyContainers(['totalPriceContainer'])
     })
 
     // On select pickup locations changing event handler 
     $("#pickupLocationsContainer").on('change', "#selectPickupLocations", evt => {
-        const [name, stops, destiny, tracks, passengers] = evt.target.value.split(' . ')
-        console.debug("On selectPickupLocations change")
+        const [name, stops, destiny, tracks, passengers] = evt.target.value.split(optionValueSeparator)
         drawTracksSelect(tracks)
 
         if (stops) {
-            drawStopsSelect(stops.split(","))
+            drawStopsSelect(stops.split(arraySeparator))
         } else {
             emptyContainers(['stopsContainer'])
         }
 
-        drawDestinySelect(destiny.split(" , "))
+        drawDestinySelect(destiny.split(arraySeparator2))
 
-        drawPassengersSelect(passengers.split(","))
+        drawPassengersSelect(passengers.split(arraySeparator))
 
         emptyContainers(['totalPriceContainer'])
     })
@@ -71,9 +73,8 @@ $(document).ready(() => {
         switch (bookType) {
             case bookTypesArr[0]:
                 try {
-                    console.debug(selectPickupLocations.value.split(' . ')[0])
                     totalPrice = prices["pickup locations"]
-                                    [selectPickupLocations.value.split(' . ')[0]]
+                                    [selectPickupLocations.value.split(optionValueSeparator)[0]]
                                     .destiny[selectDestinies.value]
                                     .tracks[tracks.value]
                                     ["passengers ranges"][selectPassengers.value]
@@ -82,12 +83,24 @@ $(document).ready(() => {
                     )
                 } catch (error) {
                     $("#totalPriceContainer").html(
-                        `<p>Consulte el precio con la empresa</p>`
+                        '<p>Consulte el precio con la empresa</p>'
                     )
                 }
                 break;
         
             case bookTypesArr[1]:
+                try {
+                    totalPrice = excursions[selectExcursions.value.split(optionValueSeparator)[0]]
+                                ["passengers ranges"][selectPassengers.value]                   
+                    
+                    $("#totalPriceContainer").html(
+                        `<p>Precio a pagar: ${totalPrice}$</p>`
+                    )
+                } catch (error) {
+                    $("#totalPriceContainer").html(
+                        '<p>Consulte el precio con la empresa</p>'
+                    )
+                }
                 break;
             
             default:
