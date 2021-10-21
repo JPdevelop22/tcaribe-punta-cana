3f1dbfa128dc4b0a08dfc5f7a94e520b34f73fb9
Jose Otniel <joseotniel2010@gmail.com>
2021-10-20 20:46:28 -0400

    Traduci el js para la pagina en ingles

---
+++ js/general-en.js
@@ -0,0 +1,205 @@
+// Globals vars
+// Timer controller for animations
+let timerId
+let carSelected // This var will change while the user click the select car
+let wameMessage = ''
+// Options series
+const optionsSerie1 = "<option value=\"\"></option>" +
+        "<option value=\"PUJ\">Punta Cana Internacional airport</option>";
+const optionsSerie2 = "<option value=\"\"></option>" + 
+        "<option value=\"HZBPC\">" + "Hotels in Bávaro, Punta Cana area" + "</option>" +
+        "<option value=\"HUA\">" + "Hotels in Uvero Alto" + "</option>" + 
+        "<option value=\"HLR\">" + "Hotels in La Romana" + "</option>" + 
+        "<option value=\"SPM\">" + "San Pedro de Macorís" + "</option>" +
+        "<option value=\"Sbo\">" + "El Seibo" + "</option>" +
+        "<option value=\"BC\">" + "Boca Chica" + "</option>" + 
+        "<option value=\"SD\">" + "Santo Domingo" + "</option>" +
+        "<option value=\"Sgo\">" + "Santiago" + "</option>" + 
+        "<option value=\"Sna\">" + "Samaná" + "</option>" + 
+        "<option value=\"PP\">" + "Puerto Plata" + "</option>";
+
+/**
+ * Draw an alert in DOM with an animation
+ * @param  {[String]} content The content to be writed in the alert
+ */
+const drawAlert = (content) => {
+    const alertContainerId = '#alertContainer'
+
+    // Look if there's a timeout running and clear it
+    if ($(alertContainerId).html()) {
+        clearTimeout(timerId)
+    }
+    
+    // First hide element and add html, then show element and put it an animation
+    $(alertContainerId).hide().html(
+        '<div class="content-one">' +
+            '<i class="fas fa-exclamation-circle"></i>' +
+        '</div>' +
+
+        '<div class="content-two">' +
+            '<p>' +
+                content +
+            '</p>' +
+        '</div>'
+    ).ready(() => {
+        $(alertContainerId).show()
+        animateCSS(alertContainerId, 'backInRight', 'faster')
+    })
+
+    // Here we're indentifing the timer for handle it
+    timerId = setTimeout(() => {
+        animateCSS(alertContainerId, 'backOutRight').then(() => {
+            $(alertContainerId).hide().empty().show()
+        })
+    }, 4000);
+}
+
+/**
+ * Function for animate elements
+ * @param  {String} element   The element query to be selected
+ * @param  {String} animation The animation to be use
+ * @param  {String} duration  The duration of the animation
+ * @param  {String} prefix    The prefix class to use
+ */
+const animateCSS = (element, animation, duration = 'fast', prefix = 'animate__') =>
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        const animationSpeed =  `${prefix}${duration}`
+        const node = document.querySelector(element);
+
+        node.classList.add(`${prefix}animated`, animationName, animationSpeed);
+
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName, animationSpeed);
+            resolve('Animation ended');
+        }
+
+        node.addEventListener('animationend', handleAnimationEnd, {once: true});
+    }
+);
+
+/**
+ * A function for format a date into a writed date
+ * @param  {Date} date The date formated to be writed
+ * @return {String}      The date in a writed format
+ */
+const formatDate = (date) => {
+    let dateToFormat = new Date(date);
+    let day = dateToFormat.getDate();
+    let year = dateToFormat.getFullYear();
+    let month = dateToFormat.toLocaleDateString("us-EN", {month: 'long'})
+
+    // Now return a writed date in Spanish
+    return month + " " + day + ", " + year; // Month Day, Year
+}
+
+/**
+ * Check errors in the reservation form, handle those errors 
+ * and return true if get an error or false wherenot.
+ * @return {bool} true if there is an error or false where not
+ */
+const checkErrors = () => {
+    const {
+        bookType,
+        pickupLocations,
+        excursions,
+        destinies,
+        passengers,
+        pickupDate
+    } = document.fBook
+
+    if (!bookType.value || !passengers.value || !pickupDate.value) {
+        drawAlert("You should fill all required inputs")
+        window.location = '#formBook'
+        return true;
+    }
+
+    if (!carSelected) {
+        drawAlert("You should select a car")
+        window.location = '#carContainer1'
+        return true;
+    }
+
+    if (bookType.value == 'Travel') {
+        if (!pickupLocations.value || !destinies.value) {
+            drawAlert("You should fill all required inputs")
+            window.location = '#formBook'
+            return true;
+        }
+
+        else if (pickupLocations.value == destinies.value) {
+            drawAlert("You should fill all required inputs")
+            window.location = '#formBook'
+            return true;
+        }
+    } else {
+        if (!excursions.value) {
+            drawAlert("You should fill all required inputs")
+            window.location = '#formBook'
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * A function for clean the value from a string that has \n and a lot of blank spaces
+ * @param  {string} txt The string to be clean
+ * @return {string}     The string cleanned
+ */
+const cleanName = (txt) => txt.replace(/\n/g, "").trim()
+
+/** A function for build the Whatsapp travel service message that will be send */
+const buildWaMeTravelMessage = () => {
+    const {
+        passengers,
+        pickupDate
+    } = document.fBook
+    const writedDate = formatDate(pickupDate.value)
+    const pickupName = cleanName($("#selectPickupLocations :selected").text())
+    const destinyName = cleanName($("#selectDestinies :selected").text())
+
+    wameMessage = 
+        `Hi!, I want to make a reservation about Travel sercvice, ` +
+        `I'd like to take the route that goes from: ${pickupName} to ` +
+        `${destinyName}, for the date of ${writedDate}, with a passenger range between ` +
+        `${passengers.value}. ` + `I chose the ${carSelected} car, ` + 'Could you offer me more information, please?'
+}
+
+/** A function for build the Whatsapp excursion service message that will be send */
+const buildWaMeExcursionMessage = () => {
+    const {
+        passengers,
+        pickupDate
+    } = document.fBook
+    const writedDate = formatDate(pickupDate.value)
+    const excursionName = cleanName($("#selectExcursions :selected").text())
+
+    wameMessage = 
+        `Hi!, I want to make a reservation about Excursion sercvice, ` +
+        `I chose the excursion called: ${excursionName}, for the date of ${writedDate} ` +
+        `with a passenger range between ${passengers.value}, ` +
+        `I chose the ${carSelected} car, ` + 'Could you offer me more information, please?'
+}
+
+
+$(document).ready(() => {
+    // pickupLocations select should change the destinies options according its value 
+    $("#selectPickupLocations").on('change', () => {
+        const pickupLocationsValue = document.fBook.pickupLocations.value
+
+        if (pickupLocationsValue != 'PUJ') {
+            if ($("#selectDestinies").html() != optionsSerie1) {
+                $("#selectDestinies").html(optionsSerie1)
+            }
+        } else {
+            if ($("#selectDestinies").html() != optionsSerie2) {
+                $("#selectDestinies").html(optionsSerie2)
+            }
+        }
+    });
+});
